f7d59c008b46c41ff9f8140764898615f1879a47
Kuma_Mon <57950327+davinchick@users.noreply.github.com>
2020-07-29 20:01:54 +0300

    Add files via upload

---
+++ langData.js
@@ -0,0 +1,345 @@
+const langData = [
+  [       
+    {
+      id: 1,
+      name: 'French',
+      species: 'France',
+      description: 'French is not only the native tongue of a major European country, but belongs to one of the most successful colonial nations in history. It is therefore spoken throughout the world, most notably in Canada and throughout western, northern and central Africa.',
+      image: 'img/french.jpg',
+      audio: 'audio/french.mp3'
+    },
+    {
+      id: 2,
+      name: 'German',
+      species: 'Germany, Austria',
+      description: 'German language has less of a global reach than the languages of some other European powers. Many of the enclaves of the German language that exist in other countries were brought by German immigrants, especially in South America and the United States.',
+      image: 'img/german.jpg',
+      audio: 'audio/german.mp3'
+    },
+    {
+      id: 3,
+      name: 'Hindi',
+      species: 'India',
+      description: 'Hindi, the main language of the South Asian nation of India, has also been called Hindustani, Urdu, and Hindi-Urdu. There are greater differences between them that can be observed in the literary forms rather than the spoken forms, which for the most part are mutually intelligible.',
+      image: 'img/hindi.jpg',
+      audio: 'audio/hindi.mp3'
+    },
+    {
+      id: 4,
+      name: 'Italian',
+      species: 'Italy',
+      description: 'Italian today is spoken worldwide by around 85 million people, including by 59 million people as a native language in the EU.  Italian was an official language in Somalia during the Colonial period, and it is still a major language in fashion, art, sports, and opera. ',
+      image: 'img/italian.jpg',
+      audio: 'audio/italian.mp3'
+    },
+    {
+      id: 5,
+      name: 'Polish',
+      species: 'Poland',
+      description: 'Polish language belongs to the Slavic languages group, along with Russian, Ukrainian, Czech or Serbian. And although between Polish and the languages of our Eastern and Southern neighbors we can find a lot of similarities, sometimes we can come across the so-called "false friends", which are words that sound alike, but have completely different meanings.',
+      image: 'img/polish.jpg',
+      audio: 'audio/polish.mp3'
+    },
+    {
+      id: 6,
+      name: 'Spanish',
+      species: 'Spain, Argentina, Chile',
+      description: 'Spanish is the second most-spoken language, second most studied language and third most used language on the internet around the world. Spanish is spoken in 44 countries (with at least 3 million native speakers in each).',
+      image: 'img/spanish.jpg',
+      audio: 'audio/spanish.mp3'
+    }
+  ],
+
+
+
+
+  [
+    {
+      id: 1,
+      name: 'Portuguese',
+      species: 'Portugal, Angola, Brazil', 
+      description: 'One of the most widely spoken languages in the world. It is also one of the fastest growing European languages, along with English and Spanish. With its humble origins in the tiny western European nation of Portugal, Portuguese is now spoken in communities across Europe, Africa, Asia, and the Americas.',
+      image: 'img/portuguese.jpg',
+      audio: 'audio/portuguese.mp3'
+    },
+    {
+      id: 2,
+      name: 'Greek',
+      species: 'Greece, Cyprus',
+      description: 'The Greek language history is divided into six periods. Each period brought some change to the language, and each had its hand in keeping it one of the world’s most lasting languages. The Greek language has absolute syntactic flexibility-elasticity, greater than any other language.',
+      image: 'img/greek.jpg',
+      audio: 'audio/greek.mp3'
+    },
+    {
+      id: 3,
+      name: 'Estonian',
+      species: 'Estonia',
+      description: 'Estonian is the native language of Estonia, spoken by almost 1.1 million Estonian people. Estonian language is pretty similar to the Finnish language, as the country has Finland as neighbour on one end, and Russia on the other. The first Estonian language book was printed in 1535.',
+      image: 'img/estonian.jpg',
+      audio: 'audio/estonian.mp3'
+    },
+    {
+      id: 4,
+      name: 'Swedish',
+      species: 'Finland',
+      description: 'Swedish was an official language in Finland before it was in Sweden. Though less than 10% of Finland’s population speaks Swedish, the government has declared it as one of the country’s official languages, along with Finnish. More words in Swedish begin with “S” than any other letter.',
+      image: 'img/swedish.jpg',
+      audio: 'audio/swedish.mp3'
+    },
+    {
+      id: 5,
+      name: 'Turkish',
+      species: 'Turky',
+      description: 'urkish had many linguistic influences from neighbouring countries as well as from European languages throughout its history. Ottoman Turkish experienced a great influx of Persian and Arabic words. There are also many loanwords of French origin.',
+      image: 'img/turkish.jpg',
+      audio: 'audio/turkish.mp3'
+    },
+    {
+      id: 6,
+      name: 'Russian',
+      species: 'Russia',
+      description: 'Russian was spread far and wide during the twentieth century due to the influence of the USSR. A collection of ancient Slavic dialects forms the basis of Russian, as well as many other Eastern European languages.',
+      image: 'img/russian.jpg',
+      audio: 'audio/russian.mp3'
+    }
+  ],
+
+
+
+
+  [
+    {
+      id: 1,
+      name: 'Armenian',
+      species: 'Armenia',
+      description: 'Armenian is widely spoken throughout the Armenian diaspora. Armenian is written in its own writing system, the Armenian alphabet, introduced in 405 AD by Mesrop Mashtots.',
+      image: 'img/armenian.jpg',
+      audio: 'audio/armenian.mp3'
+    },
+    {
+      id: 2,
+      name: 'Bulgarian',
+      species: 'Bulgaria',
+      description: 'The Bulgarian language is quite confusing the first time you encounter it; the letters are different and some of them look like the Latin but are pronounced in a completely different way. It is the oldest written Slavic language and is written in the Cyrillic alphabet.',
+      image: 'img/bulgarian.jpg',
+      audio: 'audio/bulgarian.mp3'
+    },
+    {
+      id: 3,
+      name: 'Hungarian',
+      species: 'Hungary, Croatia',
+      description: 'The Hungarian language is very rich, and has absolutely unique phonetics of the consonants. It is not an Indo-European language and has only two related languages in Europe (Finnish and Estonian).',
+      image: 'img/hungarian.jpg',
+      audio: 'audio/hungarian.mp3'
+    },
+    {
+      id: 4,
+      name: 'Czech',
+      species: 'Czech Republic',
+      description: 'The language has a formal and an informal form, it is closely related to the Slovak language. There are three main varieties of Czech language used in the Czech Republic, which are geographically defined.',
+      image: 'img/czech.jpg',
+      audio: 'audio/czech.mp3'
+    },
+    {
+      id: 5,
+      name: 'Latvian',
+      species: 'Latvia',
+      description: 'The official language of Latvia as well as one of the official languages of the European Union. There are about 1.3 million native Latvian speakers in Latvia and 100,000 abroad. The earliest documents written in Latvian were hymns written in 1530.',
+      image: 'img/latvian.jpg',
+      audio: 'audio/latvian.mp3'
+    },
+    {
+      id: 6,
+      name: 'Romanian',
+      species: 'Moldova',
+      description: 'Romanian retains a significant number of features of old Latin and also contains words taken from the surrounding Slavic languages, as well as from French, German, Greek and. Turkish.',
+      image: 'img/romanian.jpg',
+      audio: 'audio/romanian.mp3'
+    },
+    {
+      id: 7,
+      name: 'Mongolian',
+      species: 'Mongolia',
+      description: 'There are two Mongolic languages spoken in Mongolia, Oirat and Buryat. The Mongolian language has its own alphabet. Mongolian language originally didn’t have plural forms of words.',
+      image: 'img/mongolian.jpg',
+      audio: 'audio/mongolian.mp3'
+    }
+  ],
+
+
+
+  [
+    {
+      id: 1,
+      name: 'Chinese (Mandarin)',
+      species: 'China',
+      description: 'The term “Mandarin Chinese” actually refers to a vast language family made up of dozens, if not hundreds, of regional dialects in mainland China. If all these speakers are counted together, Mandarin is unequivocally the most widely-spoken language in the world',
+      image: 'img/chinaMan.jpg',
+      audio: 'audio/chinaMan.mp3'
+    },
+    {
+      id: 2,
+      name: 'Bengali',
+      species: 'Bangladesh',
+      description: 'Bengali is one of the most important languages on the Indian subcontinent, as it is the second most widely spoken language in India (after Hindi or Urdu) and the national language of the country of Bangladesh.',
+      image: 'img/bengali.jpg',
+      audio: 'audio/bengali.mp3'
+    },
+    {
+      id: 3,
+      name: 'Indonesian',
+      species: 'Indonesia',
+      description: 'Indonesian, also known as Bahasa Indonesia, is the official language of Indonesia. It is spoken as a first language by approximately 30 million of Indonesia’s inhabitants, but it is spoken as a second language by over 140 million. Indonesian is an Austronesian language and belongs to the Western, or Indonesian, branch of the language family.',
+      image: 'img/indonesian.jpg',
+      audio: 'audio/indonesian.MP3'
+    },
+    {
+      id: 4,
+      name: 'Japanese',
+      species: 'Japan',
+      description: 'The mountainous and island-based nature of the country has historically led to many pockets where dialects of the Japanese language developed in relative isolation. According to the Japanese government, all native languages spoken in Japan are dialects of Japanese and not separate languages.',
+      image: 'img/japanese.jpg',
+      audio: 'audio/japanese.mp3'
+    },
+    {
+      id: 5,
+      name: 'Korean',
+      species: 'North Korea, South Korea',
+      description: 'Korean is spoken by 80 million people worldwide. There are significant populations of Korean speakers in Brazil, Canada, Japan, Russia, the former USSR, and the US, and Korean is even one of the two official languages in China’s Yanbian Korean Autonomous Prefecture.',
+      image: 'img/korean.jpg',
+      audio: 'audio/korean.mp3'
+    },
+    {
+      id: 6,
+      name: 'Vietnamese',
+      species: 'Vietnam',
+      description: 'Vietnamese and its several dialects are spoken primarily in Vietnam and other East Asian countries, but there are large communities of Vietnamese speakers in the United States and Australia, as well. Increased travel between the Northern and Southern regions of Vietnam have led to a mixing of accents in the twentieth century.',
+      image: 'img/vietnamese.jpg',
+      audio: 'audio/vietnamese.mp3'
+    }
+  ],
+
+
+
+  [
+    {
+      id: 1,
+      name: 'Abkhaz',
+      species: 'Abkhazia',
+      description: 'Abkhaz is spoken in Abkhazia by only 100,000 people, while in Turkey there are 500,000 people speaking Abkhaz. Abkhaz is a Northwest Caucasian language, indicating it originated in the northwest Caucasus.',
+      image: 'img/????.jpg',
+      audio: 'audio/??????.mp3'
+    },
+    {
+      id: 2,
+      name: 'Albanian',
+      species: 'Albania',
+      description: 'Albanian is an Indo-European language spoken by the Albanians in the Balkans and the Albanian diaspora in the Americas, Europe and Oceania. The River Shkumbin forms the traditional divide between populations of Albania speaking two dialects of the Albanian language.',
+      image: 'img/?????.jpg',
+      audio: 'audio/?????.mp3'
+    },
+    {
+      id: 3,
+      name: 'Arabic',
+      species: 'Egypt, Iraq',
+      description: 'Both Modern Standard Arabic and the many dialects of Colloquial Arabic are descended from a mix of ancient languages spoken by the early civilizations of North Africa and the Middle East.',
+      image: 'img/arabic.jpg',
+      audio: 'audio/arabic.mp3'
+    },
+    {
+      id: 4,
+      name: 'Persian',
+      species: 'Iran',
+      description: 'The Persian language is occasionally mentioned in English as “Farsi,” but in fact this term only refers to the Iranian variant of a language that is quite widespread. Like English, Persian has no single standardized version, with the “standard” instead being determined by the country in which it is spoken.',
+      image: 'img/persian.jpg',
+      audio: 'audio/persian.mp3'
+    },
+    {
+      id: 5,
+      name: 'Punjabi',
+      species: 'Pakistan',
+      description: 'Punjabi is the most widely spoken language in Pakistan. It is by default “official language” of Sikhs all over the world, second most commonly used language in the United Kingdom, one of the eighteen national languages of India and fourth most spoken language in Canada.',
+      image: 'img/????.jpg',
+      audio: 'audio/?????.mp3'
+    },
+    {
+      id: 6,
+      name: 'Georgian',
+      species: 'Georgia',
+      description: 'Georgian language is one of the oldest languages in the world In fact, the very first reference to the Georgian language, a passage spoken by Roman grammarian Marcus Cornelius Fronto, dates back to the 2nd century AD.',
+      image: 'img/georgian.jpg',
+      audio: 'audio/georgian.mp3'
+    }
+  ],
+
+
+
+  [
+    {
+      id: 1,
+      name: 'Sanskrit',
+      species: 'India',
+      description: 'Sanskrit is an Indo-Aryan or Indic language of the ancient Indian subcontinent with a 3,500-year history. It is the primary liturgical language of Hinduism and the predominant language of most works of Hindu philosophy as well as some of the principal texts of Buddhism and Jainism.',
+      image: 'img/sanskrit.jpg',
+      audio: 'audio/sanskrit.mp3'
+    },
+    {
+      id: 2,
+      name: 'Esperanto',
+      species: 'Esperanto',
+      description: 'The more commonly-known artificial language, invented by Ludwig Zamenhof, a Jewish ophthalmologist from Poland, in 1887. Esperanto is a spoken and written blend of Latin, English, German and Romance elements and literally means "one who hopes".',
+      image: 'img/esperanto.jpg',
+      audio: 'audio/esperanto.mp3'
+    },    
+    {
+      id: 3,
+      name: 'Maori',
+      species: 'New Zealand',
+      description: 'Is an official language of New Zealand. Māori had no written language, but the symbolic meanings embodied in carving, knots and weaving were widely understood. An old legend states that the native language came to New Zealand from Hawaiki, which is a mythical land many of the Polynesian people use as the base of their culture.',
+      image: 'img/?????.jpg',
+      audio: 'audio/????????.mp3'
+    },
+    {
+      id: 4,
+      name: 'Old Greek',
+      species: ' ',
+      description: ' ',
+      image: 'img/ .jpg',
+      audio: 'audio/ .mp3'
+    },
+    {
+      id: 5,
+      name: 'Actec',
+      species: ' ',
+      description: ' ',
+      image: 'img/actec.jpg',
+      audio: 'audio/actec.mp3'
+    },
+    {
+      id: 6,
+      name: 'Suahili',
+      species: ' ',
+      description: ' ',
+      image: 'img/suahili.jpg',
+      audio: 'audio/suahili.mp3'
+    }, 
+    {
+      id: 7,
+      name: 'Udmutski',
+      species: ' ',
+      description: ' ',
+      image: 'img/udmutski.jpg',
+      audio: 'audio/udmutski.mp3'
+    },
+    {
+      id: 8,
+      name: 'valiiski',
+      species: ' ',
+      description: ' ',
+      image: 'img/valiiski.jpg',
+      audio: 'audio/valiiski.mp3'
+    }
+  ]
+];
+
+export default langData;
